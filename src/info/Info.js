c7c13aa2b24118159cf7a8661502927ec3b39825
Eetu Laine <eetulaine@gmail.com>
2023-06-12 20:20:25 +0300

    set up portfolio app again

--- src/info/Info.js
+++ src/info/Info.js
@@ -3,12 +3,11 @@ import mock1 from "../img/mock1.png"
 import mock2 from "../img/mock2.png"
 import mock3 from "../img/mock3.png"
 import mock4 from "../img/mock4.png"
-import mock5 from "../img/mock5.png"
 
 /* Hi there! Thanks for checking out my portfolio template. Be sure to read the comments to get a better understanding of
 how to make this template work best for you! */
 
-export let colors = ["rgb(0,255,164)", "rgb(166,104,255)"];
+export let colors = ["rgb(98, 208, 3)", "rgb(12, 122, 202)"];
 /*
 I highly recommend using a gradient generator like https://gradientgenerator.paytonpierce.dev/ to generate a pair of colors that you like.
 These colors will be used to style your name on the homepage, the background of your picture, and some other accents throughout
@@ -23,118 +22,111 @@ of what each of the values mean.
  */
 
 export const info = {
-    firstName: "John",
-    lastName: "Smith",
-    initials: "js", // the example uses first and last, but feel free to use three or more if you like.
-    position: "a Full Stack Developer",
+    firstName: "Eetu",
+    lastName: "Laine",
+    initials: "el", // the example uses first and last, but feel free to use three or more if you like.
+    position: "a Junior Full Stack Developer",
     selfPortrait: self, // don't change this unless you want to name your self-portrait in the "img" folder something else!
     gradient: `-webkit-linear-gradient(135deg, ${colors})`, // don't change this either
     baseColor: colors[0],
     miniBio: [ // these are just some "tidbits" about yourself. You can look at mine https://paytonjewell.github.io/#/ for an example if you'd like
         {
-            emoji: '☕',
-            text: 'fueled by coffee'
+            emoji: '🕹️',
+            text: 'avid gamer'
+        },
+        {
+            emoji: '🐶',
+            text: 'loves dogs'
         },
         {
             emoji: '🌎',
-            text: 'based in the US'
+            text: 'based in Finland'
         },
         {
             emoji: "💼",
-            text: "Systems Engineer at Google"
+            text: "Junior Developer at Digia through Academic Work"
         },
         {
             emoji: "📧",
-            text: "johnsmith@gmail.com"
+            text: "eetulaine@gmail.com"
         }
     ],
     socials: [
         {
-            link: "https://facebook.com",
-            icon: 'fa fa-facebook',
-            label: 'facebook'
+            link: "https://www.facebook.com/eetulaine",
+            icon: 'fa fa-facebook'
         },
         {
-            link: "https://instagram.com",
-            icon: 'fa fa-instagram',
-            label: 'instagram'
+            link: "https://www.instagram.com/esakaks/",
+            icon: 'fa fa-instagram'
         },
         {
-            link: "https://github.com",
-            icon: "fa fa-github",
-            label: 'github'
+            link: "https://github.com/eekaks",
+            icon: "fa fa-github"
         },
         {
-            link: "https://linkedin.com",
-            icon: "fa fa-linkedin",
-            label: 'linkedin'
+            link: "https://www.linkedin.com/in/eetu-laine-bb949a230/",
+            icon: "fa fa-linkedin"
         },
         {
-            link: "https://twitter.com",
-            icon: "fa fa-twitter",
-            label: 'twitter'
+            link: "https://twitter.com/esapeipe",
+            icon: "fa fa-twitter"
         }
 // Feel free to remove any of these that you don't have. I'll write an FAQ on how to add new ones later, let me know if you have a request for an icon!
 // Just change the links so that they lead to your social profiles.
 
     ],
-    bio: "Hello! I'm John. I'm a systems engineer for Google. I studied CompSci at Harvard, I enjoy long walks on the beach, and I believe artificial intelligence will inevitably rule us all one day. You should hire me!",
+    bio: "Hello! I'm Eetu. I'm a junior full stack developer at Digia through Academic Work. I used to teach high school English for 11 years and now I'm pursuing my dream in programming. Watch this space!",
     skills:
         {
-            proficientWith: ['javascript', 'react', 'git', 'github', 'bootstrap', 'html5', 'css3', 'figma'],
-            exposedTo: ['nodejs', 'python', 'adobe illustrator']
+            proficientWith: ['csharp', '.NET', 'reactjs', 'azure', 'python', 'sql', 'git', 'html', 'css'],
+            exposedTo: [ 'typescript', 'angular', 'nodejs', 'vue']
         }
     ,
     hobbies: [
         {
-            label: 'reading',
-            emoji: '📖'
+            label: 'football',
+            emoji: '⚽'
         },
         {
-            label: 'theater',
-            emoji: '🎭'
+            label: 'PC games',
+            emoji: '🖥️'
         },
         {
-            label: 'movies',
-            emoji: '🎥'
+            label: 'dogs',
+            emoji: '🐶'
         },
         {
-            label: 'cooking',
-            emoji: '🌶'
+            label: 'sports',
+            emoji: '👟'
         }
 // Same as above, change the emojis to match / relate to your hobbies or interests.
 // You can also remove the emojis if you'd like, I just think they look cute :P
     ],
     portfolio: [ // This is where your portfolio projects will be detailed
         {
-            title: "Project 1",
-            live: "https://paytonpierce.dev", //this should be a link to the live version of your project, think github pages, netlify, heroku, etc. Or your own domain, if you have it.
-            source: "https://github.com/paytonjewell", // this should be a link to the **repository** of the project, where the code is hosted.
-            image: mock1
-        },
-        {
-            title: "Project 2",
-            live: "https://paytonpierce.dev",
-            source: "https://github.com/paytonjewell",
+            title: "Learning Diary",
+            live: "https://learningdiary.herokuapp.com/", //this should be a link to the live version of your project, think github pages, netlify, heroku, etc. Or your own domain, if you have it.
+            source: "https://github.com/eekaks/Learning_Diary_Full_Stack", // this should be a link to the **repository** of the project, where the code is hosted.
             image: mock2
         },
         {
-            title: "Project 3",
-            live: "https://paytonpierce.dev",
-            source: "https://github.com/paytonjewell",
+            title: "Room of Terror",
+            live: "https://www.youtube.com/watch?v=2Kje8GrCYE8", //this should be a link to the live version of your project, think github pages, netlify, heroku, etc. Or your own domain, if you have it.
+            source: "https://github.com/eekaks/Pygame-project", // this should be a link to the **repository** of the project, where the code is hosted.
+            image: mock1
+        },
+        {
+            title: "Discover Helsinki",
+            live: "", //this should be a link to the live version of your project, think github pages, netlify, heroku, etc. Or your own domain, if you have it.
+            source: "https://github.com/eekaks/W5_Project_Team7", // this should be a link to the **repository** of the project, where the code is hosted.
             image: mock3
         },
         {
-            title: "Project 4",
-            live: "https://paytonpierce.dev",
-            source: "https://github.com/paytonjewell",
+            title: "Learning diary console app",
+            live: "", //this should be a link to the live version of your project, think github pages, netlify, heroku, etc. Or your own domain, if you have it.
+            source: "https://github.com/eekaks/Learning_Diary_Console_Application", // this should be a link to the **repository** of the project, where the code is hosted.
             image: mock4
         },
-        {
-            title: "Project 5",
-            live: "https://paytonpierce.dev",
-            source: "https://github.com/paytonjewell",
-            image: mock5
-        }
     ]
 }
